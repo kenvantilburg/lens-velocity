7a6a4649b944330398a3b52b2c9fecf1c996440c
crimondino <85418701+crimondino@users.noreply.github.com>
2022-01-25 17:22:50 -0500

    acc stats added

--- code/pair_catalogue/pairs_and_accels.py
+++ code/pair_catalogue/pairs_and_accels.py
@@ -1,39 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[533]:
+# In[1]:
 
 
 import numpy as np
-import myUnitsCopy1 as myU # customized library for units. All dimensional variables are in GeV and GeV=1
 import pandas as pd
 from astropy.coordinates import SkyCoord
 from astropy.coordinates import *
+from astropy.time import Time
 import astropy.units as u
-
+import healpy as hp
+from scipy.special import erf
+from scipy import stats
+import scipy as sp
 from os import listdir
 import gzip
 import sys
+
+import myUnitsCopy1 as myU # customized library for units. All dimensional variables are in GeV and GeV=1
+#import MyUnits as myU # customized library for units. All dimensional variables are in GeV and GeV=1
+
+
+# In[2]:
+
+
+#edr3_data = './edr3_data'
+#dr2_data = './dr2_data'
+#hist_res_dir = './hist_stats/'
+edr3_data = '/Users/crimondino/Dropbox (PI)/MyLensVelocity2/data/acc_catalog/edr3/'
+dr2_data = '/Users/crimondino/Dropbox (PI)/MyLensVelocity2/data/acc_catalog/dr2/'
+hist_res_dir = '/Users/crimondino/Dropbox (PI)/MyLensVelocity2/lists/hist_stats/'
+
+
+# In[3]:
+
+
+current_index=0
+
+
+# In[5]:
+
+
+### Read in the eDR3 file index from the command line
+current_index = int(sys.argv[1]) # current index in list of edr3 files
+print('\nReading in eDR3 file '+str(current_index)+'.'); sys.stdout.flush()
 
 
 # # Loading in the Files
 
 # ### Load a Single EDR3 File
 
-# In[534]:
-
-
-edr3_data = './edr3_data'
-dr2_data = './dr2_data'
-
-
-# In[535]:
+# In[6]:
 
 
 list_dr3_files = listdir(edr3_data)
 
 
-# In[536]:
+# In[7]:
 
 
 healpix_edr3_start = np.empty((len(list_dr3_files)),dtype= int)
@@ -47,7 +71,7 @@ for i,file in enumerate(list_dr3_files):
     
 
 
-# In[537]:
+# In[8]:
 
 
 def get_source_ids(file_names):
@@ -59,14 +83,14 @@ def get_source_ids(file_names):
     return start, end
 
 
-# In[538]:
+# In[9]:
 
 
 def load_dr3_file(idx):
     return pd.read_csv(edr3_data + '/' + list_dr3_files[idx], compression = 'gzip')
 
 
-# In[539]:
+# In[10]:
 
 
 start, end = get_source_ids(list_dr3_files)
@@ -74,13 +98,13 @@ start, end = get_source_ids(list_dr3_files)
 
 # ### Load Corresponding DR2 files
 
-# In[540]:
+# In[11]:
 
 
 list_dr2_files = np.array([file for file in listdir(dr2_data) if file[-7:]=='.csv.gz']) #select only files ending with 'csv.gz'
 
 
-# In[541]:
+# In[12]:
 
 
 def load_dr2_files(idx):
@@ -101,15 +125,7 @@ def load_dr2_files(idx):
 
 # ### Call the functions
 
-# In[ ]:
-
-
-### Read in the eDR3 file index from the command line
-current_index = int(sys.argv[1]) # current index in list of edr3 files
-print('\nReading in eDR3 file '+str(current_index)+'.'); sys.stdout.flush()
-
-
-# In[542]:
+# In[13]:
 
 
 dr3 = load_dr3_file(current_index)
@@ -117,7 +133,7 @@ dr2 = load_dr2_files(current_index)
 
 
 # # Generate Pair Catalogue
-# 
+
 # For each star, we first locate accidental pairs by on-sky proximity. This is the condition
 # $$|\theta_i - \theta_j| < \theta_\text{min}$$
 # where $i$ is the index of the foreground star, and $j$ is a background star for a given foreground $i$.
@@ -131,7 +147,7 @@ dr2 = load_dr2_files(current_index)
 # $$\varpi_i - n_\sigma \sigma_{\varpi_i} > \varpi_i + n_\sigma \sigma_{\varpi_j}.$$
 # This results in fewer pairs.
 
-# In[543]:
+# In[14]:
 
 
 def generate_pair_cat(df, angle_cutoff, n_sigma):
@@ -178,7 +194,7 @@ def generate_pair_cat(df, angle_cutoff, n_sigma):
 # # Generate Acceleration Catalogue
 # 
 
-# In[544]:
+# In[15]:
 
 
 def generate_pairs_list(dr3, dr2):
@@ -206,7 +222,7 @@ def generate_pairs_list(dr3, dr2):
     return result
 
 
-# In[545]:
+# In[16]:
 
 
 def propagate_back_linear(ra_g3, dec_g3, pmra_g3, pmdec_g3):
@@ -220,7 +236,7 @@ def propagate_back_linear(ra_g3, dec_g3, pmra_g3, pmdec_g3):
     return c.apply_space_motion(Time(2015.5, format='jyear'))
 
 
-# In[546]:
+# In[17]:
 
 
 def get_norm(pairs_list):
@@ -259,7 +275,7 @@ def get_norm(pairs_list):
     return pairs_list
 
 
-# In[547]:
+# In[18]:
 
 
 def match_pairs(pairs_list1):
@@ -285,7 +301,7 @@ def match_pairs(pairs_list1):
     return first_cut
 
 
-# In[548]:
+# In[19]:
 
 
 def fn_hacky_accel(th_2, th_3, mu_2, mu_3):
@@ -309,7 +325,7 @@ def fn_hacky_accel(th_2, th_3, mu_2, mu_3):
     return acc_vec
 
 
-# In[549]:
+# In[20]:
 
 
 def generate_accel_cat(dr3, dr2):
@@ -362,26 +378,164 @@ def generate_accel_cat(dr3, dr2):
     # Return minimal acceleration catalogue, with only source ID and accelerations + errors
     minimal = pair_df[['source_id','source_id.1','accel_ra', 'accel_ra_error', 'accel_dec', 'accel_dec_error']]
     minimal.columns = ['source_id_edr3', 'source_id_dr2','accel_ra', 'accel_ra_error', 'accel_dec', 'accel_dec_error']
-    return minimal
+    
+    acc_stats_col = ['source_id', 'parallax', 'parallax_error', 'accel_ra', 'accel_dec', 
+                     'accel_ra_error', 'accel_dec_error', 'phot_g_mean_mag']
+    
+    return minimal, pair_df[acc_stats_col]
+
+
+# # Acceleration statistics
+
+# In[21]:
+
+
+nside = 2**8
+fac_source_id = 2**(59-2*8)
+npix = hp.nside2npix(nside)
+#print('nside =',nside,', npix =',npix)
+#print('linear pixel size =',str(np.sqrt(4*np.pi / npix) / arcsec)[0:7],' arcsec =', str(np.sqrt(4*np.pi / npix) / degree)[0:7],' degree')
+
+
+# In[22]:
+
+
+# bin definitions
+bins_parallax = np.concatenate([[-1000],np.logspace(np.log10(0.05),np.log10(2),10),[1000]])
+#print(bins_parallax)
+bins_G = np.arange(3,23,1) # floor or the min and max G mag in the entire catalog are 3 and 21
+#print(bins_G)
+
+
+# In[23]:
+
+
+def fn_acc_stats(tab, th_count=3, return_tab=False, n_sigma_out = 3): 
+    """
+    Bins the stars in tab in healpix, G mag and parallax and computes the mean and variance of acc_ra and acc_dec per bin.
+    If return_tab=False, returns the statistic in each bin.
+    If return_tab=True, returns the stars in tab removing the outliers at more than n_sigma_out from the mean.
+    """
+
+    ### healpix binning
+    q_pix = np.floor(tab['source_id'].to_numpy() / fac_source_id).astype(int)
+    bins_pix = np.arange(np.min(np.unique(q_pix)), np.max(np.unique(q_pix))+2,1) # should be +2 to include sources in the last bin
+    q_binpix = np.digitize(q_pix, bins_pix)-1  # need to access the histogram matrix elements
+
+    ### assign to G bins
+    tab_G = tab['phot_g_mean_mag'].to_numpy()
+    q_binG = np.digitize(tab_G, bins_G)-1      
+    
+    ### probabilistic assignment to parallax bins
+    tab_parallax = tab['parallax'].to_numpy(); tab_parallax_error = tab['parallax_error'].to_numpy();
+    prob_parallax = np.nan * np.ones((len(tab),len(bins_parallax)-1))
+    for i in range(len(bins_parallax)-1):
+        x1_list = (bins_parallax[i]-tab_parallax)/tab_parallax_error/np.sqrt(2)
+        x2_list = (bins_parallax[i+1]-tab_parallax)/tab_parallax_error/np.sqrt(2)
+        prob_parallax[:,i] = 0.5*(erf(x2_list)-erf(x1_list))
+
+    tab_acc_ra = tab['accel_ra'].to_numpy(); tab_acc_dec = tab['accel_dec'].to_numpy();
+    ### histogram of summed probabilities
+    hist_prob = stats.binned_statistic_dd([tab_G,q_pix],np.transpose(prob_parallax), bins=[bins_G,bins_pix],statistic='sum')[0] 
+    ### histogram of average acc_ra weighted by probabilities
+    hist_acc_ra = stats.binned_statistic_dd([tab_G,q_pix],np.transpose(prob_parallax) * tab_acc_ra, bins=[bins_G,bins_pix],statistic='sum')[0] #sum first in each bin
+    hist_acc_ra = hist_acc_ra / (hist_prob + 1e-20) #then divide by number in each bin
+    hist_acc_dec = stats.binned_statistic_dd([tab_G,q_pix],np.transpose(prob_parallax) * tab_acc_dec, bins=[bins_G,bins_pix],statistic='sum')[0] #sum first in each bin
+    hist_acc_dec = hist_acc_dec / (hist_prob + 1e-20) #then divide by number in each bin
+    
+    ### For each star, get the mean acc of the corresponding bin
+    mean_acc_ra = hist_acc_ra[:, q_binG, q_binpix].T; mean_acc_dec = hist_acc_dec[:, q_binG, q_binpix].T
+
+    ### histogram of acc variance weighted by parallax bin probabilities
+    hist_acc_ra_var = stats.binned_statistic_dd([tab_G,q_pix],np.transpose(prob_parallax) * (mean_acc_ra.T - tab_acc_ra)**2,
+                                                   bins=[bins_G,bins_pix],statistic='sum')[0] #sum first in each bin
+    hist_acc_ra_var = hist_acc_ra_var / (hist_prob - 1 + 1e-20) # the estimator should have a -1 (this matches for example var() computed with panda's groupy)
+    hist_acc_dec_var = stats.binned_statistic_dd([tab_G,q_pix],np.transpose(prob_parallax) * (mean_acc_dec.T - tab_acc_dec)**2,
+                                                    bins=[bins_G,bins_pix],statistic='sum')[0] #sum first in each bin
+    hist_acc_dec_var = hist_acc_dec_var / (hist_prob - 1 + 1e-20) 
+    hist_acc_radec_var = stats.binned_statistic_dd([tab_G,q_pix],np.transpose(prob_parallax) * (mean_acc_ra.T - tab_acc_ra) * (mean_acc_dec.T - tab_acc_dec),
+                                                      bins=[bins_G,bins_pix],statistic='sum')[0] #sum first in each bin
+    hist_acc_radec_var = hist_acc_radec_var / (hist_prob - 1 + 1e-20) 
+    
+    ### set to nan bins where there are too few stars
+    hist_acc_ra[hist_prob < th_count] = np.nan; hist_acc_dec[hist_prob < th_count] = np.nan
+    hist_acc_ra_var[hist_prob < th_count] = np.nan; hist_acc_dec_var[hist_prob < th_count] = np.nan; hist_acc_radec_var[hist_prob < th_count] = np.nan
+
+    if return_tab==False: # returns the data frame with the statistics computed using tab
+        ###  filler for generalized bins indices
+        hist_bins_pix = np.ones(np.shape(hist_prob)) * bins_pix[:-1]
+        hist_bins_G = np.transpose(np.transpose(np.ones(np.shape(hist_prob)),axes=[0,2,1]) * bins_G[:-1],axes=[0,2,1])
+        hist_bins_parallax = np.transpose(np.transpose(np.ones(np.shape(hist_prob)),axes=[2,1,0]) * bins_parallax[:-1],axes=[2,1,0])
+
+        ###  collect data and output
+        data = np.transpose([hist_bins_pix, hist_bins_G, hist_bins_parallax, hist_prob, hist_acc_ra, hist_acc_dec, hist_acc_ra_var, hist_acc_dec_var, hist_acc_radec_var],axes=[1,2,3,0])
+        data = data.reshape(-1, data.shape[-1])
+        return pd.DataFrame(data,columns=['pix','G_bin','parallax_bin','number','mean_acc_ra','mean_acc_dec','var_acc_ra','var_acc_dec','var_acc_radec'])
+    
+    else: # returns tab where the acc outliers more than n_sigma_out away from zero have been removed
+        ### For each star, get the acc mean and variance of the corresponding bin (after excluding the low count bins)
+        mean_acc_ra = hist_acc_ra[:, q_binG, q_binpix].T; mean_acc_dec = hist_acc_dec[:, q_binG, q_binpix].T
+        var_acc_ra = hist_acc_ra_var[:, q_binG, q_binpix].T; var_acc_dec = hist_acc_dec_var[:, q_binG, q_binpix].T; var_acc_radec = hist_acc_radec_var[:, q_binG, q_binpix].T;    
+
+        ###  Get the mean and var for each star
+        tab_sum_pw = np.sum(prob_parallax, axis=1, where=(~np.isnan(mean_acc_ra)))  # sum of the parallax weights for each star using only bins with enough statistics 
+        tab_mean_acc_ra = np.sum(np.nan_to_num(mean_acc_ra*prob_parallax), axis=1)/(tab_sum_pw + 1e-20)
+        tab_mean_acc_dec = np.sum(np.nan_to_num(mean_acc_dec*prob_parallax), axis=1)/(tab_sum_pw + 1e-20)
+        tab_var_acc_ra = np.sum(np.nan_to_num(var_acc_ra*prob_parallax), axis=1)/(tab_sum_pw + 1e-20)
+        tab_var_acc_dec = np.sum(np.nan_to_num(var_acc_dec*prob_parallax), axis=1)/(tab_sum_pw + 1e-20)
+        tab_var_acc_radec = np.sum(np.nan_to_num(var_acc_radec*prob_parallax), axis=1)/(tab_sum_pw + 1e-20)        
+        
+        ### Replace the effective variance with the measurement errors for stars that have 0 mean (fall into empty bins)
+        tab_var_acc_ra[tab_var_acc_ra==0] = (tab['accel_ra_error'].to_numpy()[tab_var_acc_ra==0])**2
+        tab_var_acc_dec[tab_var_acc_dec==0] = (tab['accel_dec_error'].to_numpy()[tab_var_acc_dec==0])**2
+        tab_var_acc_radec[tab_var_acc_radec==0] = (np.zeros(len(tab))*tab['accel_ra_error'].to_numpy()*tab['accel_dec_error'].to_numpy())[tab_var_acc_radec==0]
+        
+        ### subtracted acc and inverse covariance for outlier removal
+        acc_sub = np.array([tab['accel_ra'].to_numpy()-tab_mean_acc_ra, tab['accel_dec'].to_numpy()-tab_mean_acc_dec]).T
+        inv_cov_acc = np.linalg.inv(np.array([[tab_var_acc_ra, tab_var_acc_radec], [tab_var_acc_radec, tab_var_acc_dec]]).T)
+        acc_over_sigma_sq = inv_cov_acc[:, 0, 0]*acc_sub[:, 0]**2 + inv_cov_acc[:, 1, 1]*acc_sub[:, 1]**2 + 2*inv_cov_acc[:, 0, 1]*acc_sub[:, 0]*acc_sub[:, 1]
+        
+        return tab.iloc[acc_over_sigma_sq < n_sigma_out**2]
+
+
+# In[24]:
+
+
+def fn_execute(tab, i_f, n_iter=10, flag_print=False):
+        
+    tab = tab.loc[tab['phot_g_mean_mag']>0] # only look at stars with pm and G > 0
+
+    i=0; out_frac=1
+    while (i<n_iter) & (out_frac>1E-5):
+        tab_temp = fn_acc_stats(tab, th_count=3, return_tab=True, n_sigma_out = 3)
+        i+=1; out_frac=(1-len(tab_temp)/len(tab)); 
+        if flag_print==True:
+            print('Iter '+str(i)+' -- fraction of outliers removed: '+str(out_frac*100)[:7]+' %'); sys.stdout.flush()
+        tab = tab_temp 
+        
+    df_acc_stats = fn_acc_stats(tab, return_tab=False) 
+    df_acc_stats.to_csv(hist_res_dir+list_dr3_files[i_f][:-7]+'_hist.csv', index=False) #write to file
+        
+    return len(tab)
 
 
 # # Make Both Catalogues
 
-# In[550]:
+# In[25]:
 
 
 pair_cat = generate_pair_cat(dr3, 3, 2) #cutoff at 3 arcsec, 95% CL
 
 
-# In[551]:
+# In[26]:
 
 
-accel_cat = generate_accel_cat(dr3, dr2)
+accel_cat, tab_acc_stat = generate_accel_cat(dr3, dr2)
 
 
 # ### When possible, include acceleration of background source in pair_cat
 
-# In[552]:
+# In[27]:
 
 
 def add_accel_cols(pair_cat, accel_cat):
@@ -415,13 +569,13 @@ def add_accel_cols(pair_cat, accel_cat):
     pair_cat['accel_dec_error_bg'] = accel_dec_err_to_add
 
 
-# In[553]:
+# In[28]:
 
 
 add_accel_cols(pair_cat, accel_cat)
 
 
-# # Export to CSV
+# # Export catalogues to CSV
 
 # In[554]:
 
@@ -435,4 +589,18 @@ accel_cat.to_csv('./acceleration_catalogue/'+ accel_cat_name)
 
 pair_cat_name = 'pairs_' + str(healpix_edr3_start[current_index]) +'-'+ str(healpix_edr3_end[current_index])
 pair_cat.to_csv('./accidental_pairs/' + pair_cat_name)
+
+
+# # Compute acceleration statistics and save histograms
+
+# In[32]:
+
+
+fn_execute(tab_acc_stat, current_index, n_iter=10, flag_print=False)
+
+
+# In[ ]:
+
+
+
 
